d8efd9fd2c2b46ef2baaab8df108d7748a5d67ac
obarker94 <me@oliverbarker.io>
2022-12-05 23:18:56 +0000

    tower data created

--- day/5/rust/src/main.rs
+++ day/5/rust/src/main.rs
@@ -119,30 +119,56 @@ fn get_towers(data: Vec<String>) -> () {
         .iter()
         .map(|x| x.split("").collect::<Vec<&str>>())
         .collect::<Vec<Vec<&str>>>();
-    // for each split_data remove first and last element
     split_data = split_data
         .iter()
         .map(|x| x[1..x.len() - 1].to_vec())
         .collect::<Vec<Vec<&str>>>();
 
-    // for each split_data there are 11 elements These are 3 blocks with 1 space in between for
-    // example [" ", " ", " ", " ", "[", "a", "]", " ", " ", " ", " "] should return ["", "a", ""]
+    let mut raw_lines: Vec<Vec<&str>> = vec![];
 
-    let mut parsed_lines: Vec<Vec<&str>> = vec![];
+    // remove the 4th and 8th element from split data
+    // and push the result to parsed_lines
 
-    for (_, line) in split_data.iter().enumerate() {
-        for (j, element) in line.iter().enumerate() {
-            if element == &"[" {
-                let mut parsed_line: Vec<&str> = vec![];
-                parsed_line.push("");
-                parsed_line.push(line[j + 1]);
-                parsed_line.push("");
-                parsed_lines.push(parsed_line);
+    for line in split_data {
+        let mut new_line = vec![];
+        for (index, element) in line.iter().enumerate() {
+            if index != 3 && index != 7 {
+                new_line.push(*element);
+            }
+        }
+        raw_lines.push(new_line);
+    }
+
+    let mut parsed_lines: Vec<&str> = vec![];
+
+    for line in raw_lines {
+        for (i, element) in line.iter().enumerate() {
+            if i % 3 == 1 {
+                parsed_lines.push(element);
             }
         }
     }
 
-    println!("{:?}", parsed_lines);
+    let mut chunked_data: Vec<Vec<&str>>;
+
+    chunked_data = parsed_lines
+        .chunks(3)
+        .map(|x| x.to_vec())
+        .collect::<Vec<Vec<&str>>>();
+
+    chunked_data.reverse();
+
+    let mut remapped_data: Vec<Vec<&str>> = vec![];
+
+    for i in 0..3 {
+        let mut new_line = vec![];
+        for line in chunked_data.iter() {
+            new_line.push(line[i]);
+        }
+        remapped_data.push(new_line);
+    }
+
+    println!("{:?}", remapped_data);
 }
 
 fn main() {

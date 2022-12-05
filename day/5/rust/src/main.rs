86ce9d5c9f3801e905fd7b9155703e6846848ced
obarker94 <me@oliverbarker.io>
2022-12-05 20:42:22 +0000

    instruction method created to find and parse move commands in txt file

--- day/5/rust/src/main.rs
+++ day/5/rust/src/main.rs
@@ -1,5 +1,4 @@
 #[derive(Debug)]
-
 struct Instruction {
     mov: i32,
     from: i32,
@@ -7,17 +6,37 @@ struct Instruction {
 }
 
 impl Instruction {
-    fn new(mov: i32, from: i32, to: i32) -> Instruction {
-        Instruction { mov, from, to }
+    fn new(data: Vec<String>) -> Vec<Instruction> {
+        let index = data.iter().position(|r| r == "").unwrap();
+        let instructions = data.split_at(index).1;
+        let first_element_removed = instructions[1..].to_vec();
+        let instructions_strings = first_element_removed
+            .iter()
+            .map(|x| x.split(" ").collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>();
+        let instructions = instructions_strings
+            .iter()
+            .map(|x| {
+                let data = (x[1], x[3], x[5]);
+                let (mov, from, to) = match data {
+                    (mov, from, to) => (
+                        mov.parse::<i32>().unwrap_or(0),
+                        from.parse::<i32>().unwrap_or(0),
+                        to.parse::<i32>().unwrap_or(0),
+                    ),
+                };
+
+                Instruction { mov, from, to }
+            })
+            .collect::<Vec<Instruction>>();
+
+        println!("{:?}", instructions);
+
+        instructions
     }
 }
 
 fn line_parser(line: &str) -> Result<Instruction, &str> {
-    let data_input = match std::fs::read_to_string("test_input.txt") {
-        Ok(data) => data,
-        Err(_) => return Err("Could not read test_input.txt"),
-    };
-    let test_lines = data_input.lines().collect::<Vec<_>>();
     let test = Instruction {
         mov: 0,
         from: 0,
@@ -39,6 +58,5 @@ fn main() {
         Ok(data) => data,
         Err(e) => panic!("{}", e),
     };
-
-    println!("{:?}", data);
+    let parsed_data = Instruction::new(data);
 }

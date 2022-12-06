27dc155a7f858003cf7fbc4f7e181e050e64a8f7
obarker94 <me@oliverbarker.io>
2022-12-06 23:32:53 +0000

    added new parser

--- day/5/rust/src/main.rs
+++ day/5/rust/src/main.rs
@@ -1,242 +1,169 @@
-fn split_game_data(data: Vec<String>, section_to_keep: i32) -> Vec<String> {
-    let index = data.iter().position(|r| r.is_empty()).unwrap();
-    if section_to_keep == 1 {
-        let game_data = data[..index].to_vec();
-        if game_data[0].is_empty() {
-            game_data[1..].to_vec()
-        } else {
-            game_data
-                .into_iter()
-                .filter(|line| line.contains('['))
-                .collect()
-        }
-    } else {
-        let game_data = data[index..].to_vec();
-        
-        game_data[1..].to_vec()
-    }
-}
+use std::fs;
 
 #[derive(Debug)]
-struct Instruction {
-    mov: i32,
+struct Instructions {
+    move_piece: i32,
     from: i32,
     to: i32,
 }
 
-impl Instruction {
-    fn new(data: Vec<String>) -> Vec<Instruction> {
-        let game_data = split_game_data(data, 0);
-        let instructions_strings = game_data
-            .iter()
-            .map(|x| x.split(' ').collect::<Vec<&str>>())
-            .collect::<Vec<Vec<&str>>>();
-        let instructions = instructions_strings
-            .iter()
-            .map(|x| {
-                let data = (x[1], x[3], x[5]);
-                let (mov, from, to) = match data {
-                    (mov, from, to) => (
-                        mov.parse::<i32>().unwrap_or(0),
-                        from.parse::<i32>().unwrap_or(0),
-                        to.parse::<i32>().unwrap_or(0),
-                    ),
-                };
-
-                Instruction { mov, from, to }
-            })
-            .collect::<Vec<Instruction>>();
-        instructions
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Tower<'a> {
-    stack: Vec<&'a str>,
-}
-
-impl<'a> Tower<'a> {
-    fn new() -> Tower<'a> {
-        Tower { stack: vec![] }
-    }
-
-    fn push(&mut self, value: &'a str) {
-        self.stack.push(value);
-    }
-
-    fn pop(&mut self) -> Option<&'a str> {
-        self.stack.pop()
-    }
-
-    fn peek(&self) -> Option<&'a str> {
-        self.stack.last().copied()
-    }
-
-    fn is_empty(&self) -> bool {
-        self.stack.is_empty()
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Towers<'a> {
-    towers: Vec<Tower<'a>>,
-}
-
-impl<'a> Towers<'a> {
-    fn new() -> Towers<'a> {
-        Towers {
-            towers: vec![Tower::new()],
+impl Instructions {
+    fn new(move_piece: i32, from: i32, to: i32) -> Self {
+        Self {
+            move_piece,
+            from,
+            to,
         }
     }
-
-    fn push(&mut self, value: &'a str) {
-        let last = self.towers.last_mut().unwrap();
-        last.push(value);
-    }
-
-    fn pop(&mut self) -> Option<&'a str> {
-        let last = self.towers.last_mut().unwrap();
-        last.pop()
-    }
-
-    fn remove_x_towers(&mut self, x: usize) {
-        self.towers.truncate(x);
-    }
-
-    fn initialise(&mut self, value: &'a str) {
-        self.towers.push(Tower::new());
-        self.push(value);
-    }
 }
 
-fn fetch_data(file: &str) -> Result<Vec<String>, &str> {
-    let data_input = match std::fs::read_to_string(file) {
-        Ok(data) => data.lines().map(|x| x.to_string()).collect::<Vec<String>>(),
-        Err(_) => return Err("Could not read test_input.txt"),
-    };
-    println!("Data input: {:?}", data_input);
-    Ok(data_input)
+#[derive(Debug)]
+struct FileContents {
+    towers: Vec<Vec<char>>,
+    instructions: Vec<Instructions>,
 }
 
-fn get_towers(data: Vec<String>, instructions: Vec<Instruction>) {
-    let game_data = split_game_data(data, 1);
-    let split_data = &game_data
-        .iter()
-        .map(|x| x.split("").collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>();
-
-    let purged_nulls = &split_data
-        .iter()
-        .map(|x| x[1..x.len() - 1].to_vec())
-        .collect::<Vec<Vec<&str>>>();
-
-    let mut raw_lines: Vec<Vec<&str>> = vec![];
-
-    for line in purged_nulls {
-        let mut new_line = vec![];
-        for (index, element) in line.iter().enumerate() {
-            if index != 3
-                && index != 7
-                && index != 11
-                && index != 15
-                && index != 19
-                && index != 23
-                && index != 27
-                && index != 31
-            {
-                new_line.push(*element);
-            }
+impl FileContents {
+    fn new(towers: Vec<Vec<char>>, instructions: Vec<Instructions>) -> Self {
+        FileContents {
+            towers,
+            instructions,
         }
-        raw_lines.push(new_line);
     }
+}
 
-    let mut parsed_lines: Vec<&str> = vec![];
+fn parse_instruction(line: Vec<String>, index_of_numbers: &Vec<usize>) -> Vec<i32> {
+    let mut numbers = Vec::new();
 
-    for line in raw_lines {
-        for (i, element) in line.iter().enumerate() {
-            if i % 3 == 1 {
-                parsed_lines.push(element);
+    for i in 0..index_of_numbers.len() {
+        if (i == 0 && index_of_numbers[i] + 1 == index_of_numbers[i + 1]) {
+            let merged_number = format!(
+                "{}{}",
+                line[index_of_numbers[i]],
+                line[index_of_numbers[i + 1]]
+            );
+            numbers.push(merged_number.parse::<i32>().unwrap());
+        } else if (i == 0 && index_of_numbers[1] + 1 != index_of_numbers[i + 1]) {
+            numbers.push(line[index_of_numbers[i]].parse::<i32>().unwrap());
+        } else if (i + 1 < index_of_numbers.len()
+            && index_of_numbers[i] + 1 == index_of_numbers[i + 1])
+        {
+            let merged_number = format!(
+                "{}{}",
+                line[index_of_numbers[i]],
+                line[index_of_numbers[i + 1]]
+            );
+            numbers.push(merged_number.parse::<i32>().unwrap());
+        } else {
+            if index_of_numbers[i] - 1 != index_of_numbers[i - 1] {
+                numbers.push(line[index_of_numbers[i]].parse::<i32>().unwrap());
+            } else {
+                let merged_number = format!(
+                    "{}{}",
+                    line[index_of_numbers[i - 1]],
+                    line[index_of_numbers[i]]
+                );
+                numbers.push(merged_number.parse::<i32>().unwrap());
             }
         }
     }
+    // remove duplicated values from the numbers vector
+    //
 
-    let mut chunked_data: Vec<Vec<&str>>;
-
-    chunked_data = parsed_lines
-        .chunks(9)
-        .map(|x| x.to_vec())
-        .collect::<Vec<Vec<&str>>>();
-
-    chunked_data.reverse();
-    let mut remapped_data: Vec<Vec<&str>> = vec![];
-
-    for i in 0..9 {
-        let mut new_line = vec![];
-        for line in chunked_data.iter() {
-            new_line.push(line[i]);
+    let mut unique_numbers = Vec::new();
+    for number in numbers {
+        if !unique_numbers.contains(&number) {
+            unique_numbers.push(number);
         }
-        remapped_data.push(new_line);
     }
 
-    let mut towers = Towers::new();
+    println!("unique_numbers: {:?}", unique_numbers);
 
-    for line in remapped_data {
-        for (i, element) in line.iter().enumerate() {
-            if i == 0 {
-                towers.initialise(element);
-            } else if element != &" " {
-                towers.push(element);
+    unique_numbers
+}
+
+fn read_file(path: &str) -> FileContents {
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+    let lines = contents.lines().collect::<Vec<&str>>();
+    let mut number_of_stacks: usize = 0;
+    println!("Number of stacks: {}", number_of_stacks);
+    let mut instructions: Vec<Instructions> = Vec::new();
+    let mut towers = Vec::new();
+    for line in lines {
+        let mut line_with_whitespace = line.split("").collect::<Vec<&str>>();
+        line_with_whitespace.retain(|&x| x != "");
+        line_with_whitespace.push(" ");
+        let mut instruction = Instructions::new(0, 0, 0);
+        if number_of_stacks == 0 {
+            number_of_stacks = line_with_whitespace.len() / 4;
+        }
+        let mut tower = Vec::new();
+
+        if line_with_whitespace[0] != "m" {
+            for i in 0..line_with_whitespace.len() {
+                if (i + 1) % 4 == 0 && i >= 2 {
+                    tower.push(line_with_whitespace[i - 2].chars().next().unwrap());
+                }
             }
+            towers.push(tower);
+        } else {
+            let mut index_of_numbers = Vec::new();
+            for i in 0..line_with_whitespace.len() {
+                if line_with_whitespace[i].parse::<i32>().is_ok() {
+                    index_of_numbers.push(i);
+                }
+            }
+
+            // convert line_with_whitespace to a vector of strings
+            let mut line_with_whitespace_as_strings = Vec::new();
+            for i in 0..line_with_whitespace.len() {
+                line_with_whitespace_as_strings.push(line_with_whitespace[i].to_string());
+            }
+
+            let output = parse_instruction(line_with_whitespace_as_strings, &index_of_numbers);
+
+            // for i in 0..index_of_numbers.len() {
+            //     // return numbers from index_of_numbers if two elements are next to each other (i.e. 1 and 2)
+            //     if index_of_numbers.len() > i + 1 {
+            //         if index_of_numbers[i] + 1 == index_of_numbers[i + 1] {
+            //             let combined_number = format!(
+            //                 "{}{}",
+            //                 line_with_whitespace[index_of_numbers[i]],
+            //                 line_with_whitespace[index_of_numbers[i + 1]]
+            //             );
+            //             let parsed_number = combined_number.parse::<i32>().unwrap();
+            //
+            //             if i == 0 {
+            //                 instruction.move_piece = parsed_number;
+            //                 println!("Move piece: {}", parsed_number);
+            //             } else if i == 1 {
+            //                 instruction.from = parsed_number;
+            //                 println!("From piece: {}", parsed_number);
+            //             }
+            //             println!(
+            //                 "Found numbers - {} - line - {:?}",
+            //                 parsed_number, line_with_whitespace
+            //             );
+            //         }
+            //     }
+            // }
+            instructions.push(instruction);
         }
     }
+    towers.pop();
+    towers.pop();
+    println!("Number of stacks: {}", number_of_stacks);
 
-    towers.towers.remove(0);
-
-    for instruction in instructions {
-        let (mov, from, to) = (instruction.mov, instruction.from, instruction.to);
-
-        let mut ele_to_move = "";
-        let mut eles_to_move: Vec<&str> = vec![];
-
-        for _ in 0..mov {
-            ele_to_move = towers.towers[(from - 1) as usize].pop().unwrap();
-            eles_to_move.push(ele_to_move);
-        }
-
-        eles_to_move.reverse();
-
-        for ele in eles_to_move {
-            towers.towers[(to - 1) as usize].push(ele);
-        }
-
-        // for _ in 0..mov {
-        //     ele_to_move = towers.towers[(from - 1) as usize].pop().unwrap();
-        //     towers.towers[(to - 1) as usize].push(ele_to_move);
-        // }
-
-        println!("mov: {}, from: {}, to: {}", mov, from, to);
-        println!("{:?}", towers);
-    }
-
-    let answer = &towers
-        .towers
-        .iter()
-        .map(|x| x.peek().unwrap())
-        .collect::<Vec<&str>>();
-
-    println!("{:?}", answer);
+    let file_contents = FileContents::new(towers, instructions);
+    file_contents
 }
 
 fn main() {
-    use std::time::Instant;
-    let now = Instant::now();
-    let data = match fetch_data("input.txt") {
-        Ok(data) => data,
-        Err(e) => panic!("{}", e),
-    };
-    let data_clone = data.clone();
-    let instructions = Instruction::new(data);
-    get_towers(data_clone, instructions);
-    let elapsed = now.elapsed();
-    println!("Time elapsed in function is: {:?}", elapsed);
+    let start = std::time::Instant::now();
+    let duration = start.elapsed();
+
+    let file = read_file("test_input.txt");
+    println!("{:?}", file);
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
+

a08ee9582875bff730b6aff968c0c1e1dcc7ea45
obarker94 <me@oliverbarker.io>
2022-12-12 19:08:13 +0000

    day 1 1done

--- day/11/rust/src/main.rs
+++ day/11/rust/src/main.rs
@@ -1,137 +1,117 @@
-use std::fs;
+#![feature(box_syntax)]
 
-#[derive(Debug)]
-struct Operation {
-    op: String,
-    arg: String,
-}
+use itertools::Itertools;
 
-#[derive(Debug)]
-struct Test {
-    divisible_by: u8,
-}
-
-#[derive(Debug)]
 struct Monkey {
-    id: u8,
-    items: Vec<u8>,
-    operation: Operation,
-    test: Test,
-    pass: u8,
-    fail: u8,
+    bag: Vec<usize>,
+    op: Box<dyn Fn(usize) -> usize>,
+    div: usize,
+    yay: usize,
+    nay: usize,
+    ins: usize,
 }
 
-#[allow(non_camel_case_types)]
-#[derive(Debug)]
-enum MonkeyLine {
-    id(u8),
-    items(Vec<u8>),
-    operation(Operation),
-    test(Test),
-    pass(u8),
-    fail(u8),
-}
-
-impl Monkey {
-    fn new() -> Monkey {
-        Monkey {
-            id: 0,
-            items: Vec::new(),
-            operation: Operation {
-                op: String::new(),
-                arg: String::new(),
-            },
-            test: Test { divisible_by: 0 },
-            pass: 0,
-            fail: 0,
-        }
-    }
-}
-
-fn parse_line(line: &Vec<&str>) -> MonkeyLine {
-    let extracted_data_from_line = match line[0] {
-        "Monkey" => {
-            let number = line[1].split(":").collect::<Vec<&str>>()[0]
-                .parse::<u8>()
-                .unwrap();
-            MonkeyLine::id(number)
-        }
-        "Starting" => {
-            let mut items = Vec::new();
-            for (i, item) in line.iter().enumerate() {
-                if i > 1 {
-                    let item = item.replace(",", "");
-                    let value = item
-                        .split(",")
-                        .map(|s| s.parse::<u8>().unwrap())
-                        .collect::<Vec<u8>>();
-                    items.push(value[0]);
-                }
+fn part_2() {
+    let mut m: Vec<_> = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|m| {
+            let l: Vec<_> = m.lines().map(|l| l.split(": ").last().unwrap()).collect();
+            Monkey {
+                bag: l[1].split(", ").map(|n| n.parse().unwrap()).collect(),
+                op: {
+                    let op: Vec<_> = l[2].rsplit_once("= ").unwrap().1.split(' ').collect();
+                    match op[2] {
+                        "old" => box |old| old * old,
+                        b => match (op[1], b.parse::<usize>().unwrap()) {
+                            ("+", n) => box move |old| old + n,
+                            ("*", n) => box move |old| old * n,
+                            _ => unreachable!(),
+                        },
+                    }
+                },
+                div: l[3].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                yay: l[4].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                nay: l[5].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                ins: 0,
             }
-            MonkeyLine::items(items)
-        }
-        "Operation:" => MonkeyLine::operation(Operation {
-            op: line[line.len() - 2].to_string(),
-            arg: line[line.len() - 1].to_string(),
-        }),
-        "Test:" => MonkeyLine::test(Test {
-            divisible_by: line[line.len() - 1].parse::<u8>().unwrap(),
-        }),
-        "true:" => MonkeyLine::pass(line[line.len() - 1].parse::<u8>().unwrap()),
-        "false:" => MonkeyLine::fail(line[line.len() - 1].parse::<u8>().unwrap()),
+        })
+        .collect();
+    let (mo, mut bags): (usize, _) = (m.iter().map(|m| m.div).product(), vec![vec![]; m.len()]);
 
-        _ => panic!("Invalid line"),
-    };
-    extracted_data_from_line
+    (0..10_000).for_each(|_| {
+        m.iter_mut().enumerate().for_each(|(i, m)| {
+            m.bag.append(&mut bags[i]);
+            m.bag.drain(0..).for_each(|mut n| {
+                n = (m.op)(n) % mo;
+                bags[if n % m.div == 0 { m.yay } else { m.nay }].push(n);
+                m.ins += 1;
+            });
+        });
+    });
+
+    println!(
+        "{}",
+        m.iter()
+            .map(|m| m.ins)
+            .sorted_unstable_by(|a, b| b.cmp(a))
+            .take(2)
+            .product::<usize>()
+    );
 }
 
-fn read_file(path: String) -> Vec<Monkey> {
-    let file = fs::read_to_string(path).expect("Unable to read file");
-    let mut lines = file.lines().collect::<Vec<&str>>();
-
-    lines = lines.iter().map(|s| s.trim()).collect::<Vec<&str>>();
-    lines.push("");
-
-    let mut monkeys = Vec::new();
-    let mut new_monkey = Vec::new();
-
-    for line in lines {
-        if line == "" {
-            monkeys.push(new_monkey);
-            new_monkey = Vec::new();
-        } else {
-            let mut _line = line.split(" ").collect::<Vec<&str>>();
-            if _line[0] == "If" {
-                _line.remove(0);
+pub fn part_1() {
+    let mut m: Vec<_> = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|m| {
+            let l: Vec<_> = m.lines().map(|l| l.split(": ").last().unwrap()).collect();
+            Monkey {
+                bag: l[1].split(", ").map(|n| n.parse().unwrap()).collect(),
+                op: {
+                    let op: Vec<_> = l[2].rsplit_once("= ").unwrap().1.split(' ').collect();
+                    match op[2] {
+                        "old" => box |old| old * old,
+                        b => match (op[1], b.parse::<usize>().unwrap()) {
+                            ("+", n) => box move |old| old + n,
+                            ("*", n) => box move |old| old * n,
+                            _ => unreachable!(),
+                        },
+                    }
+                },
+                div: l[3].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                yay: l[4].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                nay: l[5].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                ins: 0,
             }
-            new_monkey.push(parse_line(&_line));
-        }
-    }
+        })
+        .collect();
+    let (mo, mut bags): (usize, _) = (m.iter().map(|m| m.div).product(), vec![vec![]; m.len()]);
 
-    let mut _monkeys = Vec::new();
+    (0..20).for_each(|_| {
+        m.iter_mut().enumerate().for_each(|(i, m)| {
+            m.bag.append(&mut bags[i]);
+            m.bag.drain(0..).for_each(|mut n| {
+                n = (m.op)(n) / 3 % mo;
+                bags[if n % m.div == 0 { m.yay } else { m.nay }].push(n);
+                m.ins += 1;
+            });
+        });
+    });
 
-    for _monkey in monkeys {
-        let mut monkey = Monkey::new();
-        for line in _monkey {
-            match line {
-                MonkeyLine::id(id) => monkey.id = id,
-                MonkeyLine::items(items) => monkey.items = items,
-                MonkeyLine::operation(operation) => monkey.operation = operation,
-                MonkeyLine::test(test) => monkey.test = test,
-                MonkeyLine::pass(pass) => monkey.pass = pass,
-                MonkeyLine::fail(fail) => monkey.fail = fail,
-            }
-        }
-        println!("{:?}", monkey);
-        _monkeys.push(monkey);
-    }
-
-    _monkeys
+    println!(
+        "{}",
+        m.iter()
+            .map(|m| m.ins)
+            .sorted_unstable_by(|a, b| b.cmp(a))
+            .take(2)
+            .product::<usize>()
+    );
 }
+
 fn main() {
     let timer = std::time::Instant::now();
-    let test = read_file("test_input.txt".to_string());
-
-    let timer = timer.elapsed();
-    println!("{:?}", timer);
+    part_1();
+    part_2();
+    let elapsed = timer.elapsed();
+    println!("Elapsed: {:?}", elapsed);
 }
+
